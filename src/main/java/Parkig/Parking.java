cd04e0cad6dbbb5b20f6c6db22615d309a0ddc3a
1059234830 <1059234830@SJ106PC12.fup.edu>
2024-05-15 18:45:50 +0200

    clase  parking

--- src/main/java/Parkig/Parking.java
+++ src/main/java/Parkig/Parking.java
@@ -4,10 +4,40 @@
  */
 package Parkig;
 
-/**
- *
- * @author 1059234830
- */
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Parking {
+    private ArrayList<String> matriculas;
+    private String nombre;
+
+    public Parking(String nombre, int posicion ) {
+        this.nombre = nombre;
+        matriculas = new ArrayList<>(Collections.nCopies(posicion, null));
+        
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void entrada(String matricula, int posicion) throws ParkingException{
+        if(posicion>=matriculas.size() || posicion <0)
+            throw new ParkingException("posicion inexistente ",matricula);
+        if(matricula==null ||matricula.length()<4)
+            throw new ParkingException("matricula incorrecta ", matricula);
+        if(matriculas.get(posicion)!= null)
+            throw new ParkingException("posicion ocupada ",matricula);
+        if (matriculas.contains(matricula))
+            throw new ParkingException("matricula repetida",matricula);
+        matriculas.set(posicion, matricula);
+          
+    }
+
+    private static class ParkingException extends Exception {
+
+        public ParkingException(String posicion_inexistente_, String matricula) {
+        }
+    }
+    
     
 }
